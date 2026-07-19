35bac15ec429190118cd631eb55d240f31cc47d1
Taha Sohail <tahasohail85@gmail.com>
2026-07-19 22:11:46 +0500

    Added All buttons

--- Calculator.py
+++ Calculator.py
@@ -24,12 +24,62 @@ btn_8 = Button(root , text = '8' , bg = '#00a65a' , fg = 'white' , width = 5 , h
 btn_8.grid(row = 1 , column = 1)
 btn_8.config(font = ('verdana',14))
 
-# btn_9 = Button(root , text = '9' , bg = '#00a65a' , fg = 'white' , width = 5 , height = 2)
-# btn_9.grid(row = 1 , column = 2)
-# btn_9.config(font = ('verdana',14))
+btn_9 = Button(root , text = '9' , bg = '#00a65a' , fg = 'white' , width = 5 , height = 2)
+btn_9.grid(row = 1 , column = 2)
+btn_9.config(font = ('verdana',14))
 
-# btn_add = Button(root , text = '+' , bg = '#00a65a' , fg = 'white' , width = 5 , height = 2)
-# btn_add.grid(row = 1 , column = 0)
-# btn_add.config(font = ('verdana',14))
+btn_add = Button(root , text = '+' , bg = '#00a65a' , fg = 'white' , width = 5 , height = 2)
+btn_add.grid(row = 1 , column = 3)
+btn_add.config(font = ('verdana',14))
 
+btn_4 = Button(root , text = '4' , bg = '#00a65a' , fg = 'white' , width = 5 , height = 2)
+btn_4.grid(row = 2 , column = 0)
+btn_4.config(font = ('verdana',14))
+
+btn_5 = Button(root , text = '5' , bg = '#00a65a' , fg = 'white' , width = 5 , height = 2)
+btn_5.grid(row = 2 , column = 1)
+btn_5.config(font = ('verdana',14))
+
+btn_6 = Button(root , text = '6' , bg = '#00a65a' , fg = 'white' , width = 5 , height = 2)
+btn_6.grid(row = 2 , column = 2)
+btn_6.config(font = ('verdana',14))
+
+btn_sub = Button(root , text = '-' , bg = '#00a65a' , fg = 'white' , width = 5 , height = 2)
+btn_sub.grid(row = 2, column = 3)
+btn_sub.config(font = ('verdana',14))
+
+
+btn_3 = Button(root , text = '3' , bg = '#00a65a' , fg = 'white' , width = 5 , height = 2)
+btn_3.grid(row = 3 , column = 0)
+btn_3.config(font = ('verdana',14))
+
+btn_2 = Button(root , text = '2' , bg = '#00a65a' , fg = 'white' , width = 5 , height = 2)
+btn_2.grid(row = 3 , column = 1)
+btn_2.config(font = ('verdana',14))
+
+btn_1 = Button(root , text = '1' , bg = '#00a65a' , fg = 'white' , width = 5 , height = 2)
+btn_1.grid(row = 3 , column = 2)
+btn_1.config(font = ('verdana',14))
+
+btn_mul = Button(root , text = '*' , bg = '#00a65a' , fg = 'white' , width = 5 , height = 2)
+btn_mul.grid(row = 3 , column = 3)
+btn_mul.config(font = ('verdana',14))
+
+
+btn_C = Button(root , text = 'C' , bg = '#00a65a' , fg = 'white' , width = 5 , height = 2)
+btn_C.grid(row = 4 , column = 0)
+btn_C.config(font = ('verdana',14))
+
+btn_0 = Button(root , text = '0' , bg = '#00a65a' , fg = 'white' , width = 5 , height = 2)
+btn_0.grid(row = 4 , column = 1)
+btn_0.config(font = ('verdana',14))
+
+
+btn_eq = Button(root , text = '=' , bg = '#00a65a' , fg = 'white' , width = 5 , height = 2)
+btn_eq.grid(row = 4 , column = 2)
+btn_eq.config(font = ('verdana',14))
+
+btn_div = Button(root , text = '/' , bg = '#00a65a' , fg = 'white' , width = 5 , height = 2)
+btn_div.grid(row = 4 , column = 3)
+btn_div.config(font = ('verdana',14))
 root.mainloop()
